33f1e109f222788159e6d4e87a20b0a20aa0c45c
Aivars Lauzis <lauzis@inbox.lv>
2026-06-28 18:56:32 +0300

    feat: commit vendor folder so plugin works without composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lauzis/splecheh',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '644f0b2f3e4bcf91e5681667460dfe3c5583ee62',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'htmlburger/carbon-fields' => array(
+            'pretty_version' => 'v3.6.9',
+            'version' => '3.6.9.0',
+            'reference' => 'f82e80e3e3469d6e86cc17a8950b918ad448a059',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../htmlburger/carbon-fields',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'lauzis/splecheh' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '644f0b2f3e4bcf91e5681667460dfe3c5583ee62',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
